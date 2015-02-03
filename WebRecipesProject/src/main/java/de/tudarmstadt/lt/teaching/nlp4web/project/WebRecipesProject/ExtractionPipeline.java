3548dded08bbeacd9b310377689d7f0e1030b8aa
smndf <simon.dif.sd@gmail.com>
2015-02-03 19:34:24 +0100

    Debut evaluation

--- WebRecipesProject/src/main/java/de/tudarmstadt/lt/teaching/nlp4web/project/WebRecipesProject/ExtractionPipeline.java
+++ WebRecipesProject/src/main/java/de/tudarmstadt/lt/teaching/nlp4web/project/WebRecipesProject/ExtractionPipeline.java
@@ -15,6 +15,7 @@ import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.annotator.Di
 import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.annotator.IngredientsAnnotator;
 import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.annotator.UnitAnnotator;
 import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.reader.WebPageReader;
+import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.writer.AnalyzeResults;
 import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.writer.IngredientWriter;
 import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.writer.UnitWriter;
 import de.tudarmstadt.lt.teaching.nlp4web.project.WebRecipesProject.writer.WebPageConsumer;
@@ -23,12 +24,13 @@ import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
 
 public class ExtractionPipeline {
 
+	
 	   public static void main(String[] args)
 		    	throws UIMAException, IOException
 		    {
 		   		//http://allrecipes.com/Recipe/Alisons-Slow-Cooker-Vegetable-Beef-Soup/Detail.aspx?event8=1&prop24=SR_Thumb&e11=alison%20slow%20cooker&e8=Quick%20Search&event10=1&e7=Home%20Page&soid=sr_results_p1i1	
-		        String webpage = "http://allrecipes.com/recipe/almond-buttercrunch/";
-
+		        String webpage = "http://allrecipes.com/Recipe/Awesome-Slow-Cooker-Pot-Roast/Detail.aspx?evt19=1";
+		        String recipesFile = "src/main/resources/recipesEvaluation.txt";
 		        CollectionReader reader = createReader(
 		                WebPageReader.class,  WebPageReader.PARAM_URL, webpage 
 		        );
@@ -69,6 +71,10 @@ public class ExtractionPipeline {
 		        AnalysisEngine writer = createEngine(
 		                WebPageConsumer.class
 		        );
+		        
+		        AnalysisEngine analyzeResults = createEngine(
+		                AnalyzeResults.class, AnalyzeResults.PARAM_INPUT_FILE, recipesFile, AnalyzeResults.PARAM_URL, webpage
+		        );
 
 		        SimplePipeline.runPipeline(
 		        		reader,
@@ -80,8 +86,9 @@ public class ExtractionPipeline {
 		        		ingredientAnnotator,
 		        		directivesAnnotator,
 		        		//writer
-		        		unitWriter,
-		        		ingredientWriter
+		        		//unitWriter,
+		        		//ingredientWriter
+		        		analyzeResults
 		        		);
 		    }
 
